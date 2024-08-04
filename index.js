83fff85e8b1fb0dfcacc6e6b075f0c2db185efd5
TrulyFlawed <149352644+TrulyFlawed@users.noreply.github.com>
2024-08-04 11:44:07 -0500

    add: blue theme & author credit

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function changeTheme() {
 	else if ('light-theme' == document.body.className) {
 		document.body.className = "pink-theme";
 	}
+	else if ('pink-theme' == document.body.className) {
+		document.body.className = "blue-theme";
+	}
 	else {
 		document.body.className = "dark-theme";
 	}
