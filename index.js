9b171e4208e83497d197144eea6e12c0b0d9e3e7
TrulyFlawed <149352644+TrulyFlawed@users.noreply.github.com>
2024-08-26 20:02:05 -0500

    add: switch to random theme button

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const body = document.body;
+const themes = ["dark-theme", "light-theme", "pink-theme", "blue-theme"];
+
 function cycleTheme() {
-	const body = document.body;
-	const themes = ["dark-theme", "light-theme", "pink-theme", "blue-theme"];
 	const activeIndex = themes.findIndex((theme) => body.classList.contains(theme));
 	const nextIndex = (activeIndex + 1) % themes.length;
 
 	body.classList.replace(themes[activeIndex], themes[nextIndex]);
+}
+
+function randomTheme() {
+	const activeIndex = themes.findIndex((theme) => body.classList.contains(theme));
+	const randomTheme = Math.floor(Math.random() * themes.length);
+
+	body.classList.replace(themes[activeIndex], themes[randomTheme]);
 }
