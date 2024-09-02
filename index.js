ecc0fe90054f8af8e6671c5c0d31311dd01f7990
TrulyFlawed <149352644+TrulyFlawed@users.noreply.github.com>
2024-09-02 03:22:33 -0500

    add: specific theme selection buttons
    I would like to add that it is super early in the morning for me and I'm
    tired so I'm not going to try spending my time adding the functionality
    to add the "currently selected theme" outline to the buttons when using
    other switch methods like the "random theme" button.
    
    Instead those buttons will remove the outlines to prevent the issue of
    the buttons outlining the wrong corresponding theme button.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const body = document.body;
 const themes = ["dark-theme", "light-theme", "pink-theme", "blue-theme"];
 let activeIndex = themes.findIndex((theme) => body.classList.contains(theme));
+let themeButtons = document.querySelectorAll(".theme-buttons");
 
 function nextTheme() {
 	let nextIndex = activeIndex + 1;
@@ -8,6 +9,8 @@ function nextTheme() {
 
 	body.classList.replace(themes[activeIndex], themes[nextIndex]);
 	activeIndex = nextIndex;
+
+	themeButtons.forEach(button => button.classList.remove('active-theme'));
 }
 
 function previousTheme() {
@@ -16,6 +19,8 @@ function previousTheme() {
 
 	body.classList.replace(themes[activeIndex], themes[previousIndex]);
 	activeIndex = previousIndex;
+
+	themeButtons.forEach(button => button.classList.remove('active-theme'));
 }
 
 function randomTheme() {
@@ -23,4 +28,21 @@ function randomTheme() {
 
 	body.classList.replace(themes[activeIndex], themes[randomTheme]);
 	activeIndex = randomTheme;
+
+	themeButtons.forEach(button => button.classList.remove('active-theme'));
+}
+
+function selectTheme() {
+	themeButtons.forEach((theme =>
+		theme.addEventListener('click', () => {
+			let selectedTheme = theme.getAttribute("data-theme");
+			let selectedIndex = themes.findIndex((theme) => body.classList.contains(theme));
+
+			body.classList.replace(themes[activeIndex], selectedTheme);
+			activeIndex = selectedIndex;
+
+			themeButtons.forEach(button => button.classList.remove('active-theme'));
+			theme.classList.add("active-theme");
+		})
+	));
 }
