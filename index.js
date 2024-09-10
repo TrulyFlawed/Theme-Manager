dd65bd19834ec419a326e2c10716017b01ce0f17
TrulyFlawed <149352644+TrulyFlawed@users.noreply.github.com>
2024-09-10 10:25:05 -0500

    add: random button not selecting same theme

--- index.js
+++ index.js
@@ -2,36 +2,37 @@ const body = document.body;
 const themes = ["dark-theme", "light-theme", "pink-theme", "blue-theme"];
 let activeIndex = themes.findIndex((theme) => body.classList.contains(theme));
 let newIndex;
-let themeButtons = document.querySelectorAll(".theme-buttons");
+const themeButtonWrapper = document.querySelector(".main-theme-switches");
+const themeButtons = document.querySelectorAll(".theme-buttons");
 
 function nextTheme() {
 	newIndex = activeIndex + 1;
 	newIndex = (newIndex + themes.length) % themes.length;
-
 	updateTheme();
 }
 
 function previousTheme() {
 	newIndex = activeIndex - 1;
 	newIndex = (newIndex + themes.length) % themes.length;
-
 	updateTheme();
 }
 
 function randomTheme() {
-	newIndex = Math.floor(Math.random() * themes.length);
-
-	updateTheme();
+	randomIndex = Math.floor(Math.random() * themes.length);
+	if (randomIndex !== activeIndex) {
+		newIndex = randomIndex;
+		updateTheme();
+	}
+	else {
+		return randomTheme();
+	}
 }
 
 function selectTheme() {
-	const themeButtonWrapper = document.querySelector(".main-theme-switches");
-	
 	themeButtonWrapper.addEventListener("click", (event) => {
 		if (event.target.tagName === "BUTTON") {
 			let selectedTheme = event.target.getAttribute("data-theme");
 			newIndex = themes.indexOf(selectedTheme);
-			
 			updateTheme();
 		}
 		else {
@@ -43,7 +44,6 @@ function selectTheme() {
 function updateTheme() {
 	body.classList.replace(themes[activeIndex], themes[newIndex]);
 	activeIndex = newIndex;
-
 	let activeThemeButton = document.querySelector(`.theme-buttons[data-theme=${themes[activeIndex]}]`);
 	themeButtons.forEach(button => button.classList.remove('active-theme'));
 	activeThemeButton.classList.add("active-theme");
