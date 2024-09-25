593bdd175fee67329e02f1ef1fdc2f3f51859f21
TrulyFlawed <149352644+TrulyFlawed@users.noreply.github.com>
2024-09-25 16:48:48 -0500

    refactor: parameterize updateTheme function
    This is to remove the need for a newThemeIndex variable, which wasn't
    needed. This also makes for one less global variable to worry about.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const body = document.body;
 const siteThemes = ["dark-theme", "light-theme", "pink-theme", "blue-theme"];
 let activeThemeIndex = siteThemes.findIndex((theme) => body.classList.contains(theme));
-let newThemeIndex;
 const themeButtons = document.querySelectorAll(".theme-buttons");
 
 function arrayIndexWrapHandler(indexValue, arrayLength) {
@@ -9,34 +8,30 @@ function arrayIndexWrapHandler(indexValue, arrayLength) {
 }
 
 function nextTheme() {
-	newThemeIndex = arrayIndexWrapHandler((activeThemeIndex + 1), siteThemes.length);
-	updateTheme(newThemeIndex);
+	updateTheme(arrayIndexWrapHandler((activeThemeIndex + 1), siteThemes.length));
 }
 
 function previousTheme() {
-	newThemeIndex = arrayIndexWrapHandler((activeThemeIndex - 1), siteThemes.length);
-	updateTheme();
+	updateTheme(arrayIndexWrapHandler((activeThemeIndex - 1), siteThemes.length));
 }
 
 function randomTheme() {
 	do {
-		newThemeIndex = Math.floor(Math.random() * siteThemes.length);
+		randomThemeIndex = Math.floor(Math.random() * siteThemes.length);
 	}
-	while (newThemeIndex === activeThemeIndex);
-	updateTheme();
+	while (randomThemeIndex === activeThemeIndex);
+	updateTheme(randomThemeIndex);
 }
 
 function selectTheme() {
 	const selectedTheme = event.target.getAttribute("data-theme");
-	newThemeIndex = siteThemes.indexOf(selectedTheme);
-	updateTheme();
+	updateTheme(siteThemes.indexOf(selectedTheme));
 }
 
-function updateTheme() {
+function updateTheme(newThemeIndex) {
 	body.classList.replace(siteThemes[activeThemeIndex], siteThemes[newThemeIndex]);
 	activeThemeIndex = newThemeIndex;
 	const activeThemeButton = document.querySelector(`.theme-buttons[data-theme=${siteThemes[activeThemeIndex]}]`);
 	themeButtons.forEach(button => button.classList.remove("active-theme"));
 	activeThemeButton.classList.add("active-theme");
-	newThemeIndex = undefined;
 }
