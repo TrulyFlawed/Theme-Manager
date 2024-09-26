9d65294dcb29cbc730b22fc08a9e99bfd611012a
TrulyFlawed <149352644+TrulyFlawed@users.noreply.github.com>
2024-09-25 20:03:42 -0500

    refactor: theme logic into an IIFE
    This is to help encapsulate and organize all theme-related functions
    into a single function that can be easily modified.

--- index.js
+++ index.js
@@ -1,56 +1,58 @@
-const siteThemes = ["dark-theme", "light-theme", "pink-theme", "blue-theme"];
-let activeThemeIndex = siteThemes.findIndex((theme) => document.body.classList.contains(theme));
-const themeButtons = document.querySelectorAll(".theme-buttons");
-const themeButtonWrapper = document.querySelector(".main-theme-switches");
-
-function arrayIndexWrapHandler(indexValue, arrayLength) {
-	return (indexValue + arrayLength) % arrayLength;
-}
-
-function nextTheme() {
-	updateTheme(arrayIndexWrapHandler((activeThemeIndex + 1), siteThemes.length));
-}
-
-function previousTheme() {
-	updateTheme(arrayIndexWrapHandler((activeThemeIndex - 1), siteThemes.length));
-}
-
-function randomTheme() {
-	let randomThemeIndex;
-	do {
-		randomThemeIndex = Math.floor(Math.random() * siteThemes.length);
+const ThemeManager = (function() {
+	const siteThemes = ["dark-theme", "light-theme", "pink-theme", "blue-theme"];
+	let activeThemeIndex = siteThemes.findIndex((theme) => document.body.classList.contains(theme));
+	const themeButtons = document.querySelectorAll(".theme-buttons");
+	const themeButtonWrapper = document.querySelector(".main-theme-switches");
+	
+	function arrayIndexWrapHandler(indexValue, arrayLength) {
+		return (indexValue + arrayLength) % arrayLength;
 	}
-	while (randomThemeIndex === activeThemeIndex);
-	updateTheme(randomThemeIndex);
-}
-
-function selectTheme(event) {
-	const selectedThemeButton = event.target.closest(".theme-buttons");
-	if (selectedThemeButton) {
-		const selectedTheme = selectedThemeButton.dataset.theme;
-		updateTheme(siteThemes.indexOf(selectedTheme));
+	
+	function nextTheme() {
+		updateTheme(arrayIndexWrapHandler((activeThemeIndex + 1), siteThemes.length));
 	}
-}
-
-function updateTheme(newThemeIndex) {
-	if (newThemeIndex === activeThemeIndex) { return; }
-	else {
-		document.body.classList.replace(siteThemes[activeThemeIndex], siteThemes[newThemeIndex]);
-		activeThemeIndex = newThemeIndex;
-
-		updateThemeButtons();
+	
+	function previousTheme() {
+		updateTheme(arrayIndexWrapHandler((activeThemeIndex - 1), siteThemes.length));
 	}
-}
-
-function updateThemeButtons() {
-	themeButtons.forEach(button => button.classList.remove("active-theme"));
-	const activeThemeButton = [...themeButtons].find(button => button.dataset.theme === siteThemes[activeThemeIndex]);
-	if (activeThemeButton) {
-		activeThemeButton.classList.add("active-theme");
+	
+	function randomTheme() {
+		let randomThemeIndex;
+		do {
+			randomThemeIndex = Math.floor(Math.random() * siteThemes.length);
+		}
+		while (randomThemeIndex === activeThemeIndex);
+		updateTheme(randomThemeIndex);
 	}
-}
-
-themeButtonWrapper.addEventListener("click", selectTheme);
-document.querySelector("#previous-theme-button").addEventListener("click", previousTheme);
-document.querySelector("#random-theme-button").addEventListener("click", randomTheme);
-document.querySelector("#next-theme-button").addEventListener("click", nextTheme);
+	
+	function selectTheme(event) {
+		const selectedThemeButton = event.target.closest(".theme-buttons");
+		if (selectedThemeButton) {
+			const selectedTheme = selectedThemeButton.dataset.theme;
+			updateTheme(siteThemes.indexOf(selectedTheme));
+		}
+	}
+	
+	function updateTheme(newThemeIndex) {
+		if (newThemeIndex === activeThemeIndex) { return; }
+		else {
+			document.body.classList.replace(siteThemes[activeThemeIndex], siteThemes[newThemeIndex]);
+			activeThemeIndex = newThemeIndex;
+	
+			updateThemeButtons();
+		}
+	}
+	
+	function updateThemeButtons() {
+		themeButtons.forEach(button => button.classList.remove("active-theme"));
+		const activeThemeButton = [...themeButtons].find(button => button.dataset.theme === siteThemes[activeThemeIndex]);
+		if (activeThemeButton) {
+			activeThemeButton.classList.add("active-theme");
+		}
+	}
+	
+	themeButtonWrapper.addEventListener("click", selectTheme);
+	document.querySelector("#previous-theme-button").addEventListener("click", previousTheme);
+	document.querySelector("#random-theme-button").addEventListener("click", randomTheme);
+	document.querySelector("#next-theme-button").addEventListener("click", nextTheme);
+})()
