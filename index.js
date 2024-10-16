ba8fbca60113851eeda2b82c569c2f1eee99171b
TrulyFlawed <149352644+TrulyFlawed@users.noreply.github.com>
2024-10-16 08:43:38 -0500

    style: improve consistency

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ThemeManager = (function() {
 	let themeButtons = [];
 	let themeButtonWrapper = null;
 	let options = {};
-
+	
 	// Initialization functions
 	function initializeThemeManager(configOverrides) {
 		options = {
@@ -18,38 +18,39 @@ const ThemeManager = (function() {
 			nextButtonSelector: "#next-theme-button",
 			activeThemeClass: "active-theme",
 			// User-specified override configurations.
+			// They will override our default properties as needed.
 			...configOverrides
 		};
-
+		
 		siteThemes = options.themes;
 		activeThemeIndex = siteThemes.findIndex(theme => document.body.classList.contains(theme));
 		themeButtons = document.querySelectorAll(options.themeButtonSelector);
 		themeButtonWrapper = document.querySelector(options.buttonWrapperSelector);
-
+		
 		setupEventListeners();
-
+		
 		// Always initialize our site to load the first theme in the array.
 		// I plan to make this load whichever theme the user last selected
 		// or to set it automatically based on device/browser preferences.
 		updateTheme(0);
 	}
-
+	
 	function setupEventListeners() {
 		if (themeButtonWrapper) {
 			themeButtonWrapper.addEventListener("click", selectButtonTheme);
 		}
-
+		
 		const buttonsConfig = [
 			{ selector: options.previousButtonSelector, handler: selectPreviousTheme },
 			{ selector: options.randomButtonSelector, handler: selectRandomTheme },
 			{ selector: options.nextButtonSelector, handler: selectNextTheme },
 		];
-
+		
 		buttonsConfig.forEach(({ selector, handler }) => {
 			setupButtonEvents(selector, handler);
 		});
 	}
-
+	
 	// Helper functions
 	function setupButtonEvents(buttonSelector, buttonEventHandler) {
 		const button = document.querySelector(buttonSelector);
@@ -61,7 +62,7 @@ const ThemeManager = (function() {
 	function arrayIndexWrapHandler(indexValue, arrayLength) {
 		return (indexValue + arrayLength) % arrayLength;
 	}
-
+	
 	// Theme selection functions
 	function selectButtonTheme(event) {
 		const selectedThemeButton = event.target.closest(options.themeButtonSelector);
@@ -71,15 +72,15 @@ const ThemeManager = (function() {
 			updateTheme(siteThemes.indexOf(selectedTheme));
 		}
 	}
-
+	
 	function selectNextTheme() {
 		updateTheme(arrayIndexWrapHandler(activeThemeIndex + 1, siteThemes.length));
 	}
-
+	
 	function selectPreviousTheme() {
 		updateTheme(arrayIndexWrapHandler(activeThemeIndex - 1, siteThemes.length));
 	}
-
+	
 	function selectRandomTheme() {
 		let randomThemeIndex;
 		do {
@@ -87,30 +88,16 @@ const ThemeManager = (function() {
 		} while (randomThemeIndex === activeThemeIndex);
 		updateTheme(randomThemeIndex);
 	}
-
+	
 	// DOM update functions
 	function addThemeClass(newThemeIndex) {
 		document.body.classList.add(siteThemes[newThemeIndex]);
 	}
-
+	
 	function replaceThemeClass(newThemeIndex) {
 		document.body.classList.replace(siteThemes[activeThemeIndex], siteThemes[newThemeIndex]);
 	}
-
-	function updateTheme(newThemeIndex) {
-		if (newThemeIndex !== activeThemeIndex) {
-			if (!document.body.classList.contains(siteThemes[activeThemeIndex])) {
-				addThemeClass(newThemeIndex);
-			}
-			else {
-				replaceThemeClass(newThemeIndex);
-			}
-			activeThemeIndex = newThemeIndex;
-			updateThemeButtons();
-		}
-		else { return; }
-	}
-
+	
 	function updateThemeButtons() {
 		themeButtons.forEach(button => button.classList.remove(options.activeThemeClass));
 		const activeThemeButton = [...themeButtons].find(button => button.dataset.theme === siteThemes[activeThemeIndex]);
@@ -118,7 +105,20 @@ const ThemeManager = (function() {
 			activeThemeButton.classList.add(options.activeThemeClass);
 		}
 	}
-
+	
+	function updateTheme(newThemeIndex) {
+		if (newThemeIndex !== activeThemeIndex) {
+			// If there isn't a theme found on the document body we add a new theme class to the body element.
+			if (!document.body.classList.contains(siteThemes[activeThemeIndex])) {
+				addThemeClass(newThemeIndex);
+			} else { // Otherwise we just replace one theme class with another.
+				replaceThemeClass(newThemeIndex);
+			}
+			activeThemeIndex = newThemeIndex;
+			updateThemeButtons();
+		}
+	}
+	
 	// Public API
 	return {
 		initializeThemeManager,
